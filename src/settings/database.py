39c25c882ffcff4bfa4f47a307bc6c4f3b70a0da
black <joseb.twelve@gmail.com>
2024-05-29 21:11:26 -0500

    fix: fixed database production connection

--- src/settings/database.py
+++ src/settings/database.py
@@ -4,16 +4,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
-ENVIRONMENT = os.getenv("ENVIRONMENT")
 
-if ENVIRONMENT == "development":
-    engine = create_engine(
-        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-    )
-else:
-    engine = create_engine(
-        SQLALCHEMY_DATABASE_URL,
-    )
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
