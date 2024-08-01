5d76e9acaeedafe58e0dcb3d96f195cd771f1858
blackcoder <joseb.twelve@gmail.com>
2024-08-01 00:38:30 -0500

    Update database.py

--- src/settings/database.py
+++ src/settings/database.py
@@ -7,7 +7,7 @@ SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False},
+    #connect_args={"check_same_thread": False},
     echo=True,
 )
 
